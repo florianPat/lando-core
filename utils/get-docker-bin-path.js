39f3b79561eae73b7ff7fdabba9f68da1b4d6b0d
Florian Patruck <florian.patruck@hdnet.de>
2025-08-29 13:19:02 +0200

    Revert "feat(wsl): Use docker-engine in wsl instead of docker desktop for better stability and performance"
    This reverts commit e47222e0c0631c6ba6d08a839280b83e51f281a5.

--- utils/get-docker-bin-path.js
+++ utils/get-docker-bin-path.js
@@ -6,7 +6,6 @@ const path = require('path');
 module.exports = (platform = process.landoPlatform ?? process.platform) => {
   switch (platform) {
     case 'linux':
-    case 'wsl':
       return '/usr/share/lando/bin';
     case 'win32':
       const programFiles = process.env.ProgramW6432 || process.env.ProgramFiles;
@@ -18,6 +17,8 @@ module.exports = (platform = process.landoPlatform ?? process.platform) => {
       } else {
          return path.win32.join(programFiles + '\\Docker\\Docker\\resources\\bin');
       }
+    case 'wsl':
+      return '/mnt/wsl/docker-desktop/cli-tools/usr/bin';
     default:
       return '/usr/bin';
   }
